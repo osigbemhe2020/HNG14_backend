0d7b99e6c3938d228867d90385988b3c564b9ee6
Dirisu Paul <62507389+osigbemhe2020@users.noreply.github.com>
2026-04-30 12:56:19 +0100

    Update auth.service.js

--- services/auth.service.js
+++ services/auth.service.js
@@ -7,7 +7,7 @@ const RefreshToken = require('../models/refreshToken.model');
 
 const GITHUB_CLIENT_ID = process.env.GITHUB_CLIENT_ID;
 const GITHUB_CLIENT_SECRET = process.env.GITHUB_CLIENT_SECRET;
-const GITHUB_REDIRECT_URI = 'http://localhost:3002/auth/github/callback';
+const GITHUB_REDIRECT_URI = process.env.GITHUB_REDIRECT_URI;
 const JWT_ACCESS_SECRET = process.env.JWT_ACCESS_SECRET;
 const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET;
 
